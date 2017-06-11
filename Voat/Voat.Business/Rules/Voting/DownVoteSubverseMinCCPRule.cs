4cf2c1b67090fa9a8ff74c588297515abc059e6c
you <nevergofull@users.noreply.github.com>
2017-06-11 12:06:23 -0400

    oy can we fucking delete this sjw trash?
    oy can we fucking delete this sjw trash?

--- Voat/Voat.Business/Rules/Voting/DownVoteSubverseMinCCPRule.cs
+++ Voat/Voat.Business/Rules/Voting/DownVoteSubverseMinCCPRule.cs
@@ -1,5 +1,7 @@
 ﻿using Voat.RulesEngine;
 
+//oy can we fucking delete this sjw trash?
+
 namespace Voat.Rules.Voting
 {
     [RuleDiscovery("Approves submission downvote if user has more CCP in subverse than what is specified by the subverse minimum for downvoting.", "approved = ((subverse.minCCP > 0 and user.subverseCCP > subverse.minCCP) or subverse.minCCP == 0)")]
