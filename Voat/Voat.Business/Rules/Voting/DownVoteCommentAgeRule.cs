b6638ee0b72b8b1bf69616ee280c599de9de5e03
you <nevergofull@users.noreply.github.com>
2017-06-11 12:01:15 -0400

    Create DownVoteCommentAgeRule.cs
    lol making it 700 days to vote on a comment.  let's see how this plays out, cotton!

--- Voat/Voat.Business/Rules/Voting/DownVoteCommentAgeRule.cs
+++ Voat/Voat.Business/Rules/Voting/DownVoteCommentAgeRule.cs
@@ -5,7 +5,7 @@ using Voat.RulesEngine;
 
 namespace Voat.Rules.Voting
 {
-    [RuleDiscovery("Approved if comment is not older than 7 days", "approved = (comment.Age <= 7 days)")]
+    [RuleDiscovery("Approved if comment is not older than 700 days", "approved = (comment.Age <= 700 days)")]
     public class DownVoteCommentAgeRule : VoatRule
     {
         public DownVoteCommentAgeRule() : base("Downvote Comment Age", "2.5", RuleScope.DownVoteComment)
@@ -17,12 +17,12 @@ namespace Voat.Rules.Voting
             var q = new QueryComment(context.CommentID.Value);
             var comment = q.Execute();
 
-            // do not execute downvoting if comment is older than 7 days
+            // do not execute downvoting if comment is older than 700 days; suck it sjw puttitout!
             var commentPostingDate = comment.CreationDate;
             TimeSpan timeElapsed = Repository.CurrentDate - commentPostingDate;
-            if (timeElapsed.TotalDays > 7)
+            if (timeElapsed.TotalDays > 700)
             {
-                return CreateOutcome(RuleResult.Denied, "Comment downvotes not registered after 7 days");
+                return CreateOutcome(RuleResult.Denied, "Comment downvotes not registered after 700 days");
             }
             return base.EvaluateRule(context);
         }
